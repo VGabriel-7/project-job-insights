20b995bf722e4a5c19acc608c6f821653ee0bb0f
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-02-17 11:22:37 -0300

    Requisito 10 concluído. Implementando testes da função count_ocurrences.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+
+path = 'tests/mocks/jobs.csv'
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(path, 'back') == 1
+    assert count_ocurrences(path, 'end') == 3
+    assert count_ocurrences(path, 'developer') == 3
+    assert count_ocurrences(path, 'full') == 3
+    assert count_ocurrences(path, 'front') == 1
